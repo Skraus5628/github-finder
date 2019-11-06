8d8f5370db9ce72e5812012599601ac4731ded09
Samuel Kraus <S_read@aol.com>
2019-11-06 14:00:21 -0600

    added notes for studying conditionals

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React, { Component } from 'react';
+import React, {  Component } from 'react';
 
 import './App.css';
 
 class App extends Component {
-
+  foo = () => 'Bars';
   render(){
+     
+    const name= 'John Doe';
+    const loading = false;
+    const showName = true;
+
+    // if(loading){
+    //   return <h4>loading....</h4>
+    // }
+
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName ? name: null}</h1>}
+        {/* <h1>Hello {this.foo()}</h1> */}
+        <h1>Hello {name}</h1>
       </div>
+    
     );
   }
   
 }
 
 export default App;
+
+// Study notes:
+
+ // <Fragment> or just using <> </> can replace needing to use parent elements to render
+      //   <h1>Hello from React</h1>
+      //   <h2>Goodbye</h2>
+      // </Fragment>
+
+      // {loading ? <h4>Loading...</h4> : <h1>Hello {name}</h1>} ? if and : else conditions
+
+      // <h1>Hello {showName && name}</h1> similar to the above, the && means return only if this is true
