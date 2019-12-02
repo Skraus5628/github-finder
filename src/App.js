86987da8625d69cf245140a2430157185894cbad
Samuel Kraus <S_read@aol.com>
2019-12-02 17:49:42 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
+
+  // Lifecycle method
+componentDidMount(){
+  console.log(123);
+}
+  
   render(){
     return (
       <div className="App"> 
