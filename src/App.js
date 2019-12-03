3df9264ee61b0b23bb235c6c83eb749a9ae80e37
Samuel Kraus <S_read@aol.com>
2019-12-02 23:53:16 -0600

    created search form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {  Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -28,6 +29,7 @@ async componentDidMount(){
       <div className="App"> 
         <Navbar />
         <div className="container">
+          <Search />
         <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
