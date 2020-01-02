05194799e18412ba97d72c16107dc686555bbe5e
Samuel Kraus <S_read@aol.com>
2020-01-01 18:34:42 -0600

    Migrated main display components to a home page component, and created a not found page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, {  Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
+// import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
+// import Search from './components/users/Search';
 // import Alert from './components/layout/Alert';
+import Home from './components/Pages/Home';
 import About from './components/Pages/About';
+import NotFound from './components/Pages/NotFound';
 // import axios from 'axios';
-
 import GithubState from './Context/Github/githubState';
 import AlertState from './Context/Alert/AlertState';
 import './App.css';
@@ -21,15 +22,10 @@ const App = () =>{
         <Navbar />
         <div className='container'>
           <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                <Search />
-                <Users />
-              </Fragment>
-            )}/>
+            <Route exact path='/' component={Home}/>
             <Route exact path='/about' component={About}/>
-            <Route exact path='/user/:login' 
-            component={User}/>
+            <Route exact path='/user/:login' component={User}/>
+            <Route component ={NotFound} />
           </Switch>
         </div>
       </div>
